c5ec565e5aaabe6c4b84616bf3271c72111a1548
NamD <64962991+nd9800@users.noreply.github.com>
2020-09-12 12:19:59 +0700

    update with spellcheck

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const mssql = require("mssql");
+const config = {
+    server:'101.99.13.2',
+    database:'test',
+    user: 'sa',
+    password: 'z@GH7ytQ'
+}
+
+mssql.connect(config,function (err) {
+    if(err) console.log("Error");
+    else console.log("Connected!");
+});
+var db = new mssql.Request();
 router.get("/request",function (req,res){
     res.render("cart");
+});
+router.get("/request-complete",function (req,res){
+    res.render("request-complete");
 });
 module.exports = router;
--- routes/request-complete.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get("/request-complete",function (req,res){
-    res.render("request-complete");
-});
-module.exports = router;
