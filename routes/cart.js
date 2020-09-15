65d968960e79e7c0ba307ef083a6c4256739c976
NamD <64962991+nd9800@users.noreply.github.com>
2020-09-15 11:03:56 +0700

    update with spellcheck

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mssql = require("mssql");
+const app = express();
 const config = {
     server:'101.99.13.2',
     database:'test',
@@ -19,4 +20,20 @@ router.get("/request",function (req,res){
 router.get("/request-complete",function (req,res){
     res.render("request-complete");
 });
+
+
+
+const bodyParser =require("body-parser");
+app.use(bodyParser.urlencoded({extended:true}));
+app.post("/form-complete",function (req,res){
+    let fn = req.body.CustomerName;
+    let ln = req.body.CustomerLastname;
+    let email = req.body.CustomerEmail;
+    let phone = req.body.CustomerPhone;
+    let sql_text = "INSERT INTO T2004E_GROUP1_Customer(CustomerName,CustomerLastname,CustomerEmail,CustomerPhone) VALUES('"+fn+"','"+ln+"','"+email+"','"+phone+"')"
+    db.query(sql_text,function(err,rows){
+        if(err) res.send(err);
+        else res.redirect("/request-complete");
+    })
+})
 module.exports = router;
