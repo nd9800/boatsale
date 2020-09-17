74acf183b929b9554d1afd0868509deff148281c
Dinh Lap <63952696+kogs11@users.noreply.github.com>
2020-09-17 18:38:10 +0700

    Update linh.js

--- routes/linh.js
+++ routes/linh.js
@@ -1,12 +1,5 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
-const db = require('../model/conect')
-router.get("/",function (req,res){
-    let sql_text = `SELECT * FROM T2004E_GROUP1_Destination WHERE DestinationArea LIKE 'Mediterranean';
-                    SELECT * FROM T2004E_GROUP1_Destination WHERE DestinationArea LIKE 'Rest of the World';
-                    SELECT * FROM T2004E_GROUP1_Destination WHERE DestinationArea LIKE 'The Caribbean'`
-=======
 
 const mssql = require("mssql");
 const config = {
@@ -23,10 +16,9 @@ mssql.connect(config,function (err) {
 var db = new mssql.Request();
 router.get("/",function (req,res){
     let sql_text = "SELECT * FROM T2004E_GROUP1_Destination;SELECT * FROM T2004E_GROUP1_Destination WHERE DestinationArea LIKE 'Mediterranean';SELECT * FROM T2004E_GROUP1_Destination WHERE DestinationArea LIKE 'Rest of the World';SELECT * FROM T2004E_GROUP1_Destination WHERE DestinationArea LIKE 'The Caribbean'";
->>>>>>> 8a245ff61cbe06cca39ae5efb3c2ee910affa1bd
     db.query(sql_text, function (err,rows){
         if(err)
-            res.send("Khong co ket qua"); 
+            res.send("Khong co ket qua");
         else
             res.render("linh",{
                 des: rows.recordsets[0],
